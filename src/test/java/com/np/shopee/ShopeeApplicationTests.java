05ae862d4b8a0cf6fb47d9421a0125603da9d674
diepnguyen12 <nguyenquangdiepzed1@gmail.com>
2024-08-30 05:48:45 +0700

    initialize project

---
+++ src/test/java/com/np/shopee/ShopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.shopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
